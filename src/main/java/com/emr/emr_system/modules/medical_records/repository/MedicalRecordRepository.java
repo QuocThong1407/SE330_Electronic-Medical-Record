945946ebc82ff0cea4f51f97b0a9ae0bf58bd7ed
Truong Quoc Thong <truongquocthong07.14@gmail.com>
2026-06-12 22:54:55 +0700

    feat: add doctor's workspace include medical record and prescription board for doctor

--- src/main/java/com/emr/emr_system/modules/medical_records/repository/MedicalRecordRepository.java
+++ src/main/java/com/emr/emr_system/modules/medical_records/repository/MedicalRecordRepository.java
@@ -5,29 +5,11 @@ import com.emr.emr_system.modules.medical_records.entity.RecordStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
-public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, UUID> {
-	@Query("""
-			SELECT m
-			FROM MedicalRecord m
-			WHERE (:patientId IS NULL OR m.patientId = :patientId)
-				AND (:doctorId IS NULL OR m.doctorId = :doctorId)
-				AND ((:status IS NULL AND m.status <> com.emr.emr_system.modules.medical_records.entity.RecordStatus.ARCHIVED)
-					OR (:status IS NOT NULL AND m.status = :status))
-				AND (:startTime IS NULL OR m.visitDate >= :startTime)
-				AND (:endTime IS NULL OR m.visitDate <= :endTime)
-			""")
-	Page<MedicalRecord> searchMedicalRecords(@Param("patientId") UUID patientId,
-											 @Param("doctorId") UUID doctorId,
-											 @Param("status") RecordStatus status,
-											 @Param("startTime") LocalDateTime startTime,
-											 @Param("endTime") LocalDateTime endTime,
-											 Pageable pageable);
+public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, UUID>, JpaSpecificationExecutor<MedicalRecord> {
 
 	boolean existsByRecordNo(String recordNo);
 
