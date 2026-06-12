a2544750f82241679220ee98d2649d369ccf8a9d
ViciousEve <146147992+ViciousEve@users.noreply.github.com>
2026-06-12 22:24:39 +0700

    feat: update CORS config

--- src/main/java/com/emr/emr_system/modules/auth/security/SecurityConfig.java
+++ src/main/java/com/emr/emr_system/modules/auth/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
